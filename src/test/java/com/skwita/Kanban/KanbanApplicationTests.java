70ef8aa0c0c88364f3d66d25ba681271a773e1c8
dergachev.mv <dergachev.mv@edu.spbstu.ru>
2022-08-07 19:16:14 +0300

    Initial commit

---
+++ src/test/java/com/skwita/Kanban/KanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwita.Kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
