b17003aec83c5aa570441db6b8735349f20b9a0e
dergachev.mv <dergachev.mv@edu.spbstu.ru>
2022-08-08 15:01:45 +0300

    fixed bug

--- src/main/java/com/skwita/Kanban/model/Card.java
+++ src/main/java/com/skwita/Kanban/model/Card.java
@@ -1,13 +1,15 @@
 package com.skwita.Kanban.model;
 
 import lombok.*;
-import org.hibernate.Hibernate;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@Entity
+@NoArgsConstructor(force = true, access = AccessLevel.PUBLIC)
 public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
