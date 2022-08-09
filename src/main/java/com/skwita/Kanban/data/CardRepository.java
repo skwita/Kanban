f7cbc4be926e35a63c3c5bce710efcb0ef2e0122
dergachev.mv <dergachev.mv@edu.spbstu.ru>
2022-08-09 14:44:28 +0300

    Fixed bug 404, added search

--- src/main/java/com/skwita/Kanban/data/CardRepository.java
+++ src/main/java/com/skwita/Kanban/data/CardRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 public interface CardRepository extends CrudRepository<Card, Long> {
     List<Card> findByStatusOrderById(Status status);
     Card getCardById(long id);
-
     void deleteCardById(long id);
+    List<Card> findAllByStatusAndNameIgnoreCaseContainsOrderById(Status status, String str);
+    List<Card> findAllByStatusAndDescriptionIgnoreCaseContainsOrderById(Status status, String str);
+    List<Card> findAllByStatusAndPersonIgnoreCaseContainsOrderById(Status status, String str);
 }
--- src/main/java/com/skwita/Kanban/web/BoardController.java
+++ src/main/java/com/skwita/Kanban/web/BoardController.java
@@ -3,14 +3,17 @@ package com.skwita.Kanban.web;
 import com.skwita.Kanban.data.CardRepository;
 import com.skwita.Kanban.model.Card;
 import com.skwita.Kanban.model.Status;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j//logger
 @Controller
 public class BoardController {
     private final CardRepository cardRepository;
@@ -31,12 +34,42 @@ public class BoardController {
         return "board";
     }
 
+    @GetMapping("search")
+    public String search(Model model,
+                         @RequestParam String text,
+                         @RequestParam String field){
+        if ("name".equals(field)){
+            model.addAttribute("todoCards",
+                    cardRepository.findAllByStatusAndNameIgnoreCaseContainsOrderById(Status.TODO, text));
+            model.addAttribute("inProgressCards",
+                    cardRepository.findAllByStatusAndNameIgnoreCaseContainsOrderById(Status.INPROGRESS, text));
+            model.addAttribute("doneCards",
+                    cardRepository.findAllByStatusAndNameIgnoreCaseContainsOrderById(Status.DONE, text));
+        } else if ("description".equals(field)) {
+            model.addAttribute("todoCards",
+                    cardRepository.findAllByStatusAndDescriptionIgnoreCaseContainsOrderById(Status.TODO, text));
+            model.addAttribute("inProgressCards",
+                    cardRepository.findAllByStatusAndDescriptionIgnoreCaseContainsOrderById(Status.INPROGRESS, text));
+            model.addAttribute("doneCards",
+                    cardRepository.findAllByStatusAndDescriptionIgnoreCaseContainsOrderById(Status.DONE, text));
+        } else if ("person".equals(field)) {
+            model.addAttribute("todoCards",
+                    cardRepository.findAllByStatusAndPersonIgnoreCaseContainsOrderById(Status.TODO, text));
+            model.addAttribute("inProgressCards",
+                    cardRepository.findAllByStatusAndPersonIgnoreCaseContainsOrderById(Status.INPROGRESS, text));
+            model.addAttribute("doneCards",
+                    cardRepository.findAllByStatusAndPersonIgnoreCaseContainsOrderById(Status.DONE, text));
+        }
+
+        return "board";
+    }
+
     @GetMapping("new")
     public String createTask(@ModelAttribute Card card){
         return "new_card";
     }
 
-    @PostMapping()
+    @PostMapping("new")
     public String create(@ModelAttribute("card") @Valid Card card, Errors errors){
         if (errors.hasErrors()) {
             return "new_card";
@@ -45,14 +78,14 @@ public class BoardController {
         return "redirect:/";
     }
 
-    @GetMapping("/{id}/edit")
+    @GetMapping("{id}/edit")
     public String editCard(@PathVariable("id") long id,
                            Model model){
         model.addAttribute("card", cardRepository.getCardById(id));
         return "edit_card";
     }
 
-    @PostMapping("/{id}")
+    @PatchMapping("{id}/edit")
     public String update(@ModelAttribute("card") @Valid Card card, Errors errors){
         if (errors.hasErrors()) {
             return "edit_card";
@@ -61,13 +94,14 @@ public class BoardController {
         return "redirect:/";
     }
 
-    @DeleteMapping("/{id}")
+    @Transactional
+    @DeleteMapping("{id}/delete")
     public String deletePost(@PathVariable("id") long id){
         cardRepository.deleteCardById(id);
         return "redirect:/";
     }
 
-    @PostMapping("/{id}/move")
+    @PatchMapping("{id}/move")
     public String movePost(@PathVariable("id") long id){
         Card card = cardRepository.getCardById(id);
         if (card.getStatus() == Status.TODO) {
